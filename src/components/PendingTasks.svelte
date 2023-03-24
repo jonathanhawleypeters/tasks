5406329f65dc318d58cc0a8bf234b1675c6939b9
Jonathan Hawley-Peters <8185120+jonathanhawleypeters@users.noreply.github.com>
2023-03-23 23:06:20 -0700

    remove all done message

--- src/components/PendingTasks.svelte
+++ src/components/PendingTasks.svelte
@@ -7,15 +7,10 @@
   export let completeTask: CompleteTask;
   export let deleteTask: DeleteTask;
   export let addTask: (task: Task) => void;
-
-  $: none = !tasks.some(task => !task.completed)
 </script>
 
 <div id="current-tasks" class="section">
   <AddTask addTask={addTask} />
-  {#if none}
-    All done!
-  {/if}
   {#each tasks as task}
     {#if !task.completed}
       <Task
